f5a3d0011f37eb0a3647cdb4ac4614b885fbbe49
Cyrus Bigham <bighamcyrus@gmail.com>
2022-07-29 13:56:03 -0700

    shops pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,14 +24,9 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-<<<<<<< HEAD
-// // shops route 
-router.get('/state/shops/', async (req, res) => {
-=======
+   
 // shops route 
 router.get('/state/:id', async (req, res) => {
->>>>>>> aec5d680b29b3b7cfbe5182ac01808980c5e555a
     
   try {
     const shopData = await Shop.findAll({
@@ -48,6 +43,27 @@ router.get('/state/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Chosen shop page 
+
+router.get('/shop/:id', async (req, res) => {
+    
+  try {
+    const shopData = await Shop.findOne({
+      where: {id: req.params.id}
+    });
+
+    const shop = shopData.get({ plain: true });
+// needs to be rendered to a shops page
+    res.render('shops', {
+      shop,
+      
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 router.get('/login', (req, res) => {
 
