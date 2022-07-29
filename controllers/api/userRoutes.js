c5bcfa7c048fda036b5bdcaf3a98e3b6333d461a
RPAPE3 <106628145+RPAPE3@users.noreply.github.com>
2022-07-29 09:13:26 -0700

    saving before pulling from main

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -13,7 +13,12 @@ router.post('/', async (req, res) => {
       email: req.body.email,
       password: req.body.password
     });
-    res.status(200).json(userData);
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(userData);
+    });
   } catch (err) {
     res.status(400).json(err);
   }
